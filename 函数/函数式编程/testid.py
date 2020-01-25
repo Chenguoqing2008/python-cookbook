80f1e626ca10228bf03a80a44153d1aa26a9a3fd
Robin Chen <Robin.Chen@morningstar.com>
2020-01-25 20:30:45 +0800

    add n-gram samples

---
+++ 函数/函数式编程/testid.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+
+def testit(func, *nkwargs, **kwargs):
+    try:
+        retval = func(*nkwargs,**kwargs)
+        result = (True, retval)
+    except Exception as diag:
+        result = (False, str(diag))
+
+    return result
+
+
+def test():
+    funcs = (int,float)
+    vals = (1234, 12.34, '1234', '12.34')
+
+    for eachFunc in funcs:
+        print("_"*20)
+        for eachVal in vals:
+            retval = testit(eachFunc, eachVal)
+            if retval[0]:
+                print("{} ({}) = {}".format(eachFunc.__name__,eachVal,retval[1]))
+            else:
+                print("{} ({}) = FAILED {}".format(eachFunc.__name__, eachVal, retval[1]))
+
+
+if __name__ == '__main__':
+    test()
